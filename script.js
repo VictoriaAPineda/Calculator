bef3ce100f430f6eb67e86dc69bcc8dc8379a1cf
Victoria <piklad1996@gmail.com>
2023-05-09 18:06:51 -0500

    js code added

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function show(num){
+    document.getElementById("output").value += num;
+    /* the += adds on the next number beside it*/
+    
+}
+
+function clear(){
+    document.getElementById("output").value = " ";
+    /* retrieves the listed values in the output bar and sets it to empty string*/
+}
