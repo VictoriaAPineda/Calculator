2c7c288518bcef7bb909345c7cb84e87a46ad189
Victoria <piklad1996@gmail.com>
2023-05-09 22:13:58 -0500

    update

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 
 
 function show(num){
-    document.getElementById("output").value += num;
+    document.getElementById("output-bar").value += num;
     /* the += appends on the next number beside it*/
     
 }
 
-function clear(){
-    document.getElementById("output").value ="";
+/* used to be clear() but that is a pre-made js method and can't be used, got error*/
+function clearOutput(){
+    document.getElementById("output-bar").value = "";
     /* retrieves the listed value in the output bar and sets it to empty string*/
-    
-
 }
 
 function calc(){
-    let num = document.getElementById("output").value;
+    let num = document.getElementById("output-bar").value;
     let result = eval(num);
-    document.getElementById("output").value = result;
+    document.getElementById("output-bar").value = result;
 }
