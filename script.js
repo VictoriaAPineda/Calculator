67be7619c4df6f38926c8eb12188469bc45ff6cf
Victoria <piklad1996@gmail.com>
2023-05-09 21:21:50 -0500

    add

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 
 function show(num){
     document.getElementById("output").value += num;
-    /* the += adds on the next number beside it*/
+    /* the += appends on the next number beside it*/
     
 }
 
 function clear(){
-    document.getElementById("output").value = " ";
-    /* retrieves the listed values in the output bar and sets it to empty string*/
+    document.getElementById("output").value ="";
+    /* retrieves the listed value in the output bar and sets it to empty string*/
+    
+
+}
+
+function calc(){
+    let num = document.getElementById("output").value;
+    let result = eval(num);
+    document.getElementById("output").value = result;
 }
